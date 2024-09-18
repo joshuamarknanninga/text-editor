35849802a1f8943a3d5c111f16df4d5d3eeab416
Joshua Nanninga <jmarknanninga@gmail.com>
2024-09-18 12:51:58 -0500

    added index.js

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -5,3 +5,12 @@ import '../css/styles.css';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/monokai.css';
 
+// Check if service workers are supported
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      });
+    });
+  }
+  
