4ae3bb21bedc344846d6b70ee95c094a7eafab96
Joshua Nanninga <jmarknanninga@gmail.com>
2024-09-18 13:48:19 -0500

    updated, debugged

--- server/routes/html.Routes.js
+++ server/routes/html.Routes.js
@@ -2,20 +2,7 @@ const path = require('path');
 const router = require('express').Router();
 const htmlController = require('../controllers/htmlController');
 
-// Serve the index.html file for the root route
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../dist/index.html'));
-});
-
-// Catch-all route to serve index.html for any unrecognized routes
-router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../dist/index.html'));
-});
-
-// Root route
-router.get('/', htmlController.getIndex);
-
-// Catch-all route
+// Serve the index.html file for the root and catch-all routes
 router.get('*', htmlController.getIndex);
 
 module.exports = router;
